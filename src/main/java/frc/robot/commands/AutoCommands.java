694489da094bb61a6ab394ed04deee0cae2b83a0
Omkaarcodes <145941839+Omkaarcodes@users.noreply.github.com>
2025-01-18 14:06:40 -0500

    Baisc commands required for autos; more to be added.

---
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -0,0 +1,75 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.drive.Drive;
+
+import com.ctre.phoenix6.CANBus;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.pathfinding.Pathfinding;
+import com.pathplanner.lib.util.PathPlannerLogging;
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.pathplanner.lib.path.PathConstraints;
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants.Mode;
+import frc.robot.generated.TunerConstants;
+import frc.robot.util.LocalADStarAK;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class AutoCommands{
+
+    //Constants for below commands
+
+    static PathConstraints constraints = new PathConstraints(
+        3.0, 4.0,
+        Units.degreesToRadians(540), Units.degreesToRadians(720));
+
+    
+    public static Command resetPosetoStartofPath(Pose2d pose, PathPlannerPath path, Drive drive) {
+        return Commands.runOnce(() -> drive.setPose((path.getStartingDifferentialPose())), drive);   
+
+    }
+
+    public static Command pathOnTheFlytoPose (Pose2d targetPose) {
+
+        Command pathfindingCommand = AutoBuilder.pathfindToPose(
+                targetPose,
+                constraints,
+                0.0 );
+
+        return pathfindingCommand;
+    }
+
+
+}
+
