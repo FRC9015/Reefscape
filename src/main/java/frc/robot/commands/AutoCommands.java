6c4cad0877f28cb1a2ea8b6558134edf00e957f5
Omkaarcodes <145941839+Omkaarcodes@users.noreply.github.com>
2025-01-25 13:13:27 -0500

    added command

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -1,79 +1,48 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.*;
-import frc.robot.subsystems.drive.Drive;
-
-import com.ctre.phoenix6.CANBus;
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.ModuleConfig;
-import com.pathplanner.lib.config.PIDConstants;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.pathfinding.Pathfinding;
-import com.pathplanner.lib.util.PathPlannerLogging;
-import edu.wpi.first.hal.FRCNetComm.tInstances;
-import edu.wpi.first.hal.FRCNetComm.tResourceType;
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Twist2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.Alert;
-import edu.wpi.first.wpilibj.Alert.AlertType;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import com.pathplanner.lib.path.PathConstraints;
-import edu.wpi.first.math.util.Units;
-import frc.robot.Constants.Mode;
-import frc.robot.generated.TunerConstants;
-import frc.robot.util.LocalADStarAK;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import frc.robot.subsystems.drive.Drive;
 
+public class AutoCommands {
 
-public class AutoCommands{
+  // Constants for below commands
 
-    //Constants for below commands
+  public static PathConstraints constraints =
+      new PathConstraints(3.0, 4.0, Units.degreesToRadians(540), Units.degreesToRadians(720));
 
-    public static PathConstraints constraints = new PathConstraints(
-        3.0, 4.0,
-        Units.degreesToRadians(540), Units.degreesToRadians(720));
+  public static Command resetPosetoStartofPath(Pose2d pose, PathPlannerPath path, Drive drive) {
+    return Commands.runOnce(() -> drive.setPose((path.getStartingHolonomicPose().get())), drive);
+  }
 
+  public static Command pathOnTheFlytoPose(Pose2d targetPose) {
+
+    Command pathfindingCommand = AutoBuilder.pathfindToPose(targetPose, constraints, 0.0);
+    return pathfindingCommand;
+  }
+
+  public static Command correcToDesiredPath(PathPlannerPath desiredPath) {
+    Command correctToDesiredPath = AutoBuilder.pathfindThenFollowPath(desiredPath, constraints);
+    return correctToDesiredPath;
+  }
+
+  public Command pathfindThenFollowPath(String pathName) {
+    try {
+    PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
     
-    public static Command resetPosetoStartofPath(Pose2d pose, PathPlannerPath path, Drive drive) {
-        return Commands.runOnce(() -> drive.setPose((path.getStartingHolonomicPose().get())), drive);   
-
-    }
-
-    public static Command pathOnTheFlytoPose (Pose2d targetPose) {
-
-        Command pathfindingCommand = AutoBuilder.pathfindToPose(
-                targetPose,
-                constraints,
-                0.0 );
-
-        return pathfindingCommand;
-    }
-    
-    public static Command correcToDesiredPath(PathPlannerPath desiredPath) {
-        Command correctToDesiredPath = AutoBuilder.pathfindThenFollowPath(desiredPath, constraints);
-        return correctToDesiredPath;
-
-    }
-
+    return AutoBuilder.pathfindThenFollowPath(
+        path,
+        new PathConstraints(3, 3, Math.PI / 2, Math.PI / 2)
+    );
+  }
+  catch(Exception e) {
+    return Commands.runOnce(() -> System.out.println("Path not found: " + pathName));
+  }
 }
 
+}
