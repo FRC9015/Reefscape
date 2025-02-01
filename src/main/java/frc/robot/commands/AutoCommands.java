cab9707520a908efd8420a8c155105f2cc2b1646
Omkaarcodes <145941839+Omkaarcodes@users.noreply.github.com>
2025-01-31 19:14:40 -0500

    Added capability to warmup endeffector and intake motors while running path-on-the-fly.

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -1,13 +1,22 @@
 package frc.robot.commands;
 
+import java.util.List;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.endeffector.EndEffector;
+import frc.robot.subsystems.intake.Intake;
 
 public class AutoCommands {
 
@@ -43,6 +52,28 @@ public class AutoCommands {
   catch(Exception e) {
     return Commands.runOnce(() -> System.out.println("Path not found: " + pathName));
   }
+  
 }
+
+public PathPlannerPath returnFirstPathinAuto(String autoName) {
+        try {
+            List<PathPlannerPath> pathCollection = PathPlannerAuto.getPathGroupFromAutoFile(autoName);
+            return pathCollection.get(0);
+        }
+        catch(Exception e) {
+            System.out.println("Path not found: " + autoName + "Exception: " + e);
+            return null;
+        }
+    }
+
+public Command pathfindToAutoStartPoseWhileWarmup(Pose2d currentPose, String desiredAuto, EndEffector endEffector, Intake intake) {
+        Pose2d startPose = returnFirstPathinAuto(desiredAuto).getStartingHolonomicPose().get();
+
+        return new ParallelCommandGroup(
+            AutoBuilder.pathfindToPose(startPose, AutoConstants.PP_CONSTRAINTS, returnFirstPathinAuto(desiredAuto).getIdealStartingState().velocityMPS()),
+            Commands.runOnce(()->endEffector.setRPM(6000), endEffector),
+            Commands.runOnce(()->intake.setRPM(6000), intake)
+        );
+    }
 
 }
