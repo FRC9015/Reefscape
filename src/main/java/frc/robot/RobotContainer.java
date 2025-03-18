1512703407031619b2619f26f660186cf9d24bed
Omkaar Sampigeadi <145941839+Omkaarcodes@users.noreply.github.com>
2025-03-17 21:51:26 -0400

    started adding shuffleboard widgets (wip)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
@@ -370,6 +372,18 @@ public class RobotContainer {
     coralFound.and(() -> DriverStation.isTeleopEnabled()).whileTrue(endEffector.runEffector(2));
 
     // Pathfind to source
+
+  }
+
+  public void displayMatchData() {
+    Shuffleboard.getTab("MatchData")
+        .add("Sensor Indicator", coralFound.getAsBoolean()).withWidget(BuiltInWidgets.kBooleanBox);
+    Shuffleboard.getTab("MatchData")
+        .add("Match Timer", DriverStation.getMatchTime()).withWidget(BuiltInWidgets.kTextView);
+    Shuffleboard.getTab("MatchData")
+        .add(elavatorCamera).withSize(4, 4);
+    Shuffleboard.getTab("MatchData")
+        .addCamera("Bow Camera", "Bow", null);
 
   }
 
